68b6732e9bf680247032535e8620760e61bd8716
LaobanAndMumu <sunyuching@msn.com>
2017-03-29 13:07:41 -0400

    Add files via upload

--- node_server.js
+++ node_server.js
@@ -7,6 +7,7 @@ var database;
 
 var port = 5000;
 var result=[];
+var findRecord;
 
 app.use(express.static(__dirname+'/frontend'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,12 +25,12 @@ app.get('/search', function (req, res){
 	var findDict2 = {};
 	
 	if(inputList[0] == 'amazon_video'){
-		findDict1 = {'reviewText':{$regex:inputList[1]}};
-		findDict2 = {_id:0,reviewerID:1,reviewerName:1,reviewText:1,reviewTime:1};
+		findDict1 = {'reviewText':{$regex:inputList[1],$options:'i'}};
+		findDict2 = {_id:0,reviewerID:1,reviewerName:1,reviewText:1,reviewTime:1,comments:1};
 	}
 	else{
-		findDict1 = {'tweet':{$regex:inputList[1]}};
-		findDict2 = {_id:0,fromUser:1,fromUserId:1,tweet:1,createdAt:1};
+		findDict1 = {'tweet':{$regex:inputList[1],$options:'i'}};
+		findDict2 = {_id:0,fromUser:1,fromUserId:1,tweet:1,createdAt:1,comments:1};
 	}
 	
 	console.log(findDict1);
@@ -45,7 +46,9 @@ app.get('/search', function (req, res){
 		
 		//(function(err, result) {
 			//console.log(items);
+			
 			result.then(function(something){
+				console.log(something);
 			res.json(something);
 			//console.log(something);
 			});
@@ -53,6 +56,60 @@ app.get('/search', function (req, res){
 	});
     
 });
+
+app.post('/post', function (req, res){
+	var databaseUse=req.body.whichDatabase;
+	var userId = req.body.id;
+	var comment = req.body.comment;
+	
+	//amazon 
+	if (databaseUse == 'amazon_video'){
+		database.collection(databaseUse, function(err, collection) {
+		
+		console.log('collection');
+		
+		// insert a comment to the comments attribute
+		collection.update( { reviewerID: userId },
+							{ $push: { comments: comment } }
+						 );
+		
+		
+		//{'tweet':{$regex:searchWord}},{_id:0,fromUser:1,fromUserId:1,tweet:1,createdAt:1}).toArray();
+		
+		//(function(err, result) {
+			//console.log(items);
+			//result.then(function(something){
+			//res.json(something);
+			//console.log(something);
+			//});
+		//});
+		});
+	}//if
+	else{
+		database.collection(databaseUse, function(err, collection) {
+		
+		console.log('collection');
+		
+		// insert a comment to the comments attribute
+		collection.update( { fromUserId: userId },
+							{ $push: { comments: comment } }
+						 )
+		
+		
+		//{'tweet':{$regex:searchWord}},{_id:0,fromUser:1,fromUserId:1,tweet:1,createdAt:1}).toArray();
+		
+		//(function(err, result) {
+			//console.log(items);
+			//result.then(function(something){
+			//res.json('');
+			//console.log(something);
+		});
+		//});
+		}//else
+	});
+	
+	
+	
 
 
 // Retrieve
